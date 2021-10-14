e30ce0778f35fdc6d96831ad29ef6aabdb14b405
vasilyevev <vasilyevev@ozon.ru>
2021-10-14 15:23:16 +0300

    Add webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     mode: 'development',
@@ -19,5 +20,11 @@ module.exports = {
                 }
             }
         ]
-    }
+    },
+
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: 'client/pages/main.html'
+        }),
+    ],
 }
